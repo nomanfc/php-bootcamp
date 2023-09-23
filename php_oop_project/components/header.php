d8537066bbd068a59d7a5d88ce2dd4bf53923087
nomanfc <nomanfcb@gmail.com>
2023-09-23 22:05:20 +0600

    Php files: footer, header, main_content added

---
+++ php_oop_project/components/header.php
@@ -0,0 +1,4 @@
+    <div class="header">
+        <h2>OOP in PHP..... Hello</h2>
+    </div>
+
---
+++ php_oop_project/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+  <title>PHP OOP</title>
+  <link rel="stylesheet" href="css/styles.css" />
+</head>
+
+<body>
+  <?php include  "components/header.php" ?>
+  <div class="main_content">
+    <?php include "components/main_content.php" ?>
+  </div>
+  <?php include  "components/footer.php" ?>
+</body>
+
+</html>
