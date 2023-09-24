a639e9d0198449b4191c2c2786264b3fae04e5a9
nomanfc <nomanfcb@gmail.com>
2023-09-24 22:57:39 +0600

    constant file updated

--- php_oop_project/components/constant.php
+++ php_oop_project/components/constant.php
@@ -1,23 +1,27 @@
 <h4 style="color: red">
-    How to access constant with scope resolution (::)  in Php?
+    How to access constant with scope resolution (::) in Php?
 </h4>
 
 <?php
-class ConstantProperty {
+class ConstantProperty
+{
     const NAME = "Abdullah Al-Noman";
 
 
-    public function display(){
-        echo "Accessing constant with display method (class_name::constant_name): ".ConstantProperty::NAME."<br/>";
-        echo "Accessing constant with display method (self::constant_name): ".self::NAME."<br/>";
+    public function display()
+    {
+        echo "Accessing constant with display method (class_name::constant_name): " . ConstantProperty::NAME . "<br/>";
+        echo "Accessing constant with display method (self::constant_name): " . self::NAME . "<br/>";
     }
 }
 
 
 $obj = new ConstantProperty();
-
+echo "<b>Inside Class</b> <br/>";
 $obj->display();
+echo "<b>Outside Class</b>";
+echo "<br/>Accessing constant with object (obj_name::constant_name): " . $obj::NAME . "<br/>";
+echo "Accessing constant directly from class without object (class_name::constant_name): " . ConstantProperty::NAME . "<br/>";
 
-echo "Accessing constant with object: ".$obj::NAME."<br/>";
 
 ?>
